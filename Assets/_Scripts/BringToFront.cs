59b582c57b5ba59941fa4e028b108200e782dbb9
Tanay Nigam <tanaynigam12@gmail.com>
2020-02-18 00:31:09 -0500

    First Commit

---
+++ Assets/_Scripts/BringToFront.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BringToFront : MonoBehaviour
+{
+
+    public float distance;
+    private bool isObjectInFront;
+    private GameObject objectInFront;
+    private Vector3 originalPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isObjectInFront = false;   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.touchCount > 0)
+        {
+            if (isObjectInFront)
+            {
+                objectInFront.transform.position = originalPosition;
+                isObjectInFront = false;
+            }
+            else
+            {
+                Touch touch = Input.GetTouch(0);
+                RaycastHit hit;
+                Ray ray = Camera.main.ScreenPointToRay(touch.position);
+                if (Physics.Raycast(ray, out hit, 2000))
+                {
+                    if (hit.transform.tag == "Frames")
+                    {
+                        originalPosition = hit.transform.position;
+                        hit.transform.position = Camera.main.transform.position + Camera.main.transform.forward * distance;
+                        objectInFront = hit.transform.gameObject;
+                    }
+                }
+                isObjectInFront = true;
+            }
+        }
+
+    }
+}
